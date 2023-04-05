02afbd0d81a7fc8a21fc9b9e6ee00ce3307e45c8
Werlley Gonçalves <werlley.registros@hotmail.com>
2023-04-04 21:52:14 -0300

    feat: toastify config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,9 @@
 import '@/styles/globals.scss';
 import type { AppProps } from 'next/app';
 
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 import { Roboto } from 'next/font/google';
 const roboto = Roboto({
   weight: ['300', '400', '100', '500', '700'],
@@ -12,6 +15,18 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <main className={roboto.className}>
       <Component {...pageProps} />
+      <ToastContainer
+        position='top-right'
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme='light'
+      />
     </main>
   );
 }
